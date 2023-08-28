a581a9ffab6d492895fef6a0c8aae44687689bd0
aka.chilala <aka.chilala@trademe.co.nz>
2023-08-28 21:07:43 +1200

    Made download file more testable but adding more arguments

--- download_atp_tour_data.py
+++ download_atp_tour_data.py
@@ -19,72 +19,72 @@ atp_tour = duckdb.connect("atp_tour.duckdb")
 atp_tour.sql("INSTALL httpfs")
 atp_tour.sql("LOAD httpfs")
 
-def create_schema(schema_name: str = "raw") -> None:
+def create_schema_if_not_exists(schema_name: str) -> None:
     atp_tour.execute(
         f"""
         create schema if not exists {schema_name}
         """
     )
 
-def read_atp_players_csv_file(url: str) -> None:
-    atp_players_csv_file = f"{url}/atp_players.csv?raw=true"
+def read_atp_players_csv_file(base_url: str, schema_name: str) -> None:
+    atp_players_csv_file = f"{base_url}/atp_players.csv?raw=true"
     # display files to download
     log.debug(colored(f"downloading atp players file: {atp_players_csv_file}", 'blue'))
-    # make sure schema for raw data exists
-    create_schema()
-    # import players into raw schema
+    # make sure schema exists
+    create_schema_if_not_exists(schema_name=schema_name)
+    # import players data
     atp_tour.execute(
-        """
-        create or replace table raw.players as
+        f"""
+        create or replace table {schema_name}.players as
         select *
           from read_csv_auto($atp_players_csv_file)
         """,
-        {"atp_players_csv_file": atp_players_csv_file}
+        {
+            "atp_players_csv_file": atp_players_csv_file
+        }
     )
     # tidying-up strings that cannot be converted to dates
     # i.e 19000000 there's neither a month of 00 nor a date of 00 so defaulting to 1st of Jan
     atp_tour.execute(
-        """
-        alter table raw.players
+        f"""
+        alter table {schema_name}.players
             alter dob set data type date using cast(strptime(cast(regexp_replace(dob, '0000$', '0101') as string), '%Y%m%d') as date)
         """
     )
 
-def read_atp_matches_csv_files(url: str) -> None:
-    # read csv data from Github in this format i.e https://github.com/JeffSackmann/tennis_atp/blob/master/atp_matches_1968.csv
-    # using a list comprehension we can loop through these files
-    # https://www.w3schools.com/python/python_lists_comprehension.asp
+def read_atp_matches_csv_files(base_url: str, schema_name: str, year_from: int, year_to: int) -> None:
+    """
+    read csv data from Github in this format i.e https://github.com/JeffSackmann/tennis_atp/blob/master/atp_matches_1968.csv
+    using a list comprehension we can loop through these files
+    https://www.w3schools.com/python/python_lists_comprehension.asp
+    """
     atp_matches_csv_files =[
-        f"{url}/atp_matches_{year}.csv?raw=true"
-        for year in range(1968, 2023)
+        f"{base_url}/atp_matches_{year}.csv?raw=true"
+        for year in range(year_from, year_to + 1)
     ]
     # display files to download
     log.debug(colored(f"downloading atp matches files: {atp_matches_csv_files}", 'green'))
-    # make sure schema for raw data exists
-    create_schema();
-    # import matches into raw schema
+    # make sure schema exists
+    create_schema_if_not_exists(schema_name=schema_name)
+    # import matches data
     atp_tour.execute(
-        """
-        create or replace table raw.matches as
+        f"""
+        create or replace table {schema_name}.matches as
         select *
-          from read_csv_auto(
-            $1,
-            types={
-                "winner_seed": varchar,
-                "loser_seed": varchar
-            }
-        )
+          from read_csv_auto($atp_matches_csv_files)
         """,
-        [atp_matches_csv_files]
+        {
+            "atp_matches_csv_files": atp_matches_csv_files
+        }
     )
     atp_tour.execute(
-        """
-        alter table raw.matches
+        f"""
+        alter table {schema_name}.matches
             alter tourney_date set data type date using cast(strptime(cast(tourney_date as string), '%Y%m%d') as date)
         """
     )
 
-def download_and_read_countries_json_file() -> None:
+def download_and_read_countries_json_file(schema_name: str) -> None:
     url = 'https://restcountries.com/v3.1/all'
     # display files to download
     log.debug(colored(f"downloading countries json file: {url}", 'magenta'))
@@ -94,22 +94,36 @@ def download_and_read_countries_json_file() -> None:
     json_file = '.countries.json'
     with open(json_file, 'w') as file:
         json.dump(json_data, file, indent=4)
-
+    # make sure schema exists
+    create_schema_if_not_exists(schema_name=schema_name)
     # import matches into raw schema
     atp_tour.execute(
-        """
-        create or replace table raw.countries as
+        f"""
+        create or replace table {schema_name}.countries as
         select *
           from read_json_auto($json_file)
         """,
-        {"json_file": json_file}
+        {
+            "json_file": json_file
+        }
     )
 
 def main() -> None:
     base_url = "https://github.com/JeffSackmann/tennis_atp/blob/master"
-    read_atp_players_csv_file(url=base_url)
-    read_atp_matches_csv_files(url=base_url)
-    download_and_read_countries_json_file()
+    schema_name = "raw"
+    read_atp_players_csv_file(
+        base_url=base_url,
+        schema_name=schema_name
+    )
+    read_atp_matches_csv_files(
+        base_url=base_url,
+        schema_name=schema_name,
+        year_from=1968,
+        year_to=2023
+    )
+    download_and_read_countries_json_file(
+        schema_name=schema_name
+    )
 
 if __name__ == "__main__":
     main()
