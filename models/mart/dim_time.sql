2ca9077930b5716ba2c597df3316f718a04dbcdd
aka.chilala <aka.chilala@trademe.co.nz>
2023-10-12 00:19:28 +1300

    Refactored the unknown record to appear first

--- models/mart/dim_time.sql
+++ models/mart/dim_time.sql
@@ -38,15 +38,31 @@ with recursive num_of_minutes as (
             when this_minute >= '06:00:00' and this_minute < '12:00:00' then 'Morning'
             when this_minute >= '12:00:00' and this_minute < '18:00:00' then 'Afternoon'
             else 'Evening'
-         end:: varchar as time_of_day
+         end::varchar as time_of_day
         ,case
             when this_minute >= '13:00:00' then this_minute - interval 12 hours
             else this_minute
          end::varchar as notation_12
-        ,this_minute:: varchar as notation_24
+        ,this_minute::varchar as notation_24
     from time_series
 )
 , unknown_record as (
+  select unknown_key::int as dim_time_key
+        ,null::interval as the_time
+        ,unknown_text::varchar as standard_time
+        ,unknown_integer::int as num_of_minutes
+        ,unknown_integer::int as the_hour
+        ,unknown_integer::int as the_minute
+        ,unknown_integer::int as military_hour
+        ,unknown_integer::int as military_minute
+        ,unknown_text::varchar as am_pm
+        ,unknown_text::varchar as time_of_day
+        ,unknown_text::varchar as notation_12
+        ,unknown_text::varchar as notation_24
+    from ref_unknown_record
+
+  union all
+
   select dim_time_key
         ,the_time
         ,notation_12||' '||am_pm as standard_time
@@ -60,22 +76,6 @@ with recursive num_of_minutes as (
         ,notation_12
         ,notation_24
     from renamed
-
-  union all
-
-  select unknown_key as dim_time_key
-        ,null as the_time
-        ,unknown_text as standard_time
-        ,unknown_integer as num_of_minutes
-        ,unknown_integer as the_hour
-        ,unknown_integer as the_minute
-        ,unknown_integer as military_hour
-        ,unknown_integer as military_minute
-        ,unknown_text as am_pm
-        ,unknown_text as time_of_day
-        ,unknown_text as notation_12
-        ,unknown_text as notation_24
-    from ref_unknown_record
 )
 select *
   from unknown_record
